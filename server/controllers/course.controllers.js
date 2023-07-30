281bb7ea325ca869625558475a4e2adaeb10589d
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-07-30 15:14:52 +0530

    Practice

--- server/controllers/course.controllers.js
+++ server/controllers/course.controllers.js
@@ -1,5 +1,40 @@
 import Course from "../models/course.model.js";
+import AppError from "../utils/appError.js";
+
 
 export const getAllCourses = async (req , res, next) =>{
+    try {
 
+        const courses = await Course.find().select('-lectures').lean().exec();
+        res.status(200).json({
+            success : true,
+            message : 'All Courses',
+            courses
+        })
+
+    } catch (e) {
+        return next( new AppError (e.message, 500));
+    }
+}
+
+export const getLecturesByCourseId = async (req, res, next) =>{
+    try {
+        
+        const { courseId } = req.params;
+        const course = await Course.findById({_id:courseId}).lean().exec();
+
+        if(!course){
+            return next(new AppError('Invalid course Id', 400))
+        }
+
+        res.status(200).json({
+            success : true,
+            message : 'All Lectures fetched Successfully',
+            lectures : course.lectures
+        })
+
+
+    } catch (e) {
+        return next( new AppError(e.message, 500));
+    }
 }
--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import { getAllCourses, getLecturesByCourseId } from '../controllers/course.controllers.js';
+import IsLoggedIn from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
 router
     .route('/')
     .get(getAllCourses);
+
+router
+    .route('/:courseId')
+    .get( IsLoggedIn ,getLecturesByCourseId);
 
 
 export default router;
