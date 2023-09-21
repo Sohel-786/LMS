82405866b8a27aa942aa05842b2380cbdcfdd8a6
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-21 22:31:09 +0530

    revision

--- server/app.js
+++ server/app.js
@@ -1,40 +1,45 @@
-import {config} from 'dotenv';
+import { config } from "dotenv";
 config();
-import express from 'express';
+import express from "express";
 
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import morgon from 'morgan';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgon from "morgan";
 
-import userRoutes from './routes/user.routes.js';
-import errMiddleware from'./middlewares/error.middleware.js';
-import courseRoutes from './routes/course.routes.js';
-import paymentRoutes from './routes/payment.routes.js';
+import userRoutes from "./routes/user.routes.js";
+import errMiddleware from "./middlewares/error.middleware.js";
+import courseRoutes from "./routes/course.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(morgon('dev'));
+app.use(morgon("dev"));
 
-app.use(cors({
-    origin : [process.env.FRONTEND_URL],
-    credentials : true
-}));
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 
-app.use('/ping', (req, res) =>{
-    res.send('Pong');
+app.use("/ping", (req, res) => {
+  res.send("Pong");
 });
 
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/courses", courseRoutes);
+app.use("/api/v1/payments", paymentRoutes);
 
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/courses', courseRoutes);
-app.use('/api/v1/payments', paymentRoutes);
-
-app.all('*', (req, res) =>{
-    res.status(404).send('<h1 style=" width:100%; text-align:center;">OOPS!! 404 page not found</h1>');
-})
+app.all("*", (req, res) => {
+  res
+    .status(404)
+    .send(
+      '<h1 style=" width:100%; text-align:center;">OOPS!! 404 page not found</h1>'
+    );
+});
 
 app.use(errMiddleware);
 
--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
-import app  from './app.js';
+import app from "./app.js";
 
-import connect from './config/db.js';
+import connect from "./config/db.js";
 
-import cloudinary from 'cloudinary';
+import cloudinary from "cloudinary";
 
-import Razorpay from 'razorpay';
+import Razorpay from "razorpay";
 
 cloudinary.v2.config({
-    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
-    api_secret:  process.env.CLOUDINARY_API_SECRET,
-    api_key : process.env.CLOUDINARY_API_KEY
-})
-
-export const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_SECRET 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_API_KEY,
 });
 
-app.listen(process.env.PORT, () =>{
-    connect();
-    console.log(`Listening on ${process.env.PORT}`)
-})
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
+});
+
+app.listen(process.env.PORT, () => {
+  connect();
+  console.log(`Listening on ${process.env.PORT}`);
+});
