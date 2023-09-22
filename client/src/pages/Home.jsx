ad454e4bbf9bb15e8352fc9f2724e4915fd99742
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-22 22:27:47 +0530

    Building Pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,16 +5,27 @@ function Home() {
   return (
     <HomeLayout>
       <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
-        <div className="w-1/2 space-x-6">
+        <div className="w-1/2 space-y-6">
+
           <h1 className="text-5xl font-semibold">
             Find Out Best{" "}
             <span className="text-yellow-500 font-bold">Online Courses</span>
           </h1>
 
-          <div className="space-x-6">
+          <p className="text-xl">
+            We have a large library of courses taught by highly skilled and qualified faculties at a very affordable cost.
+          </p>
+
+          <div className="flex gap-7">
             <Link to="/courses">
-              <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg">
+              <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
                 Expolore Courses
+              </button>
+            </Link>
+
+            <Link>
+            <button className="bg-transparent px-5 py-3 border-2 border-yellow-600 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out duration-300">
+                Contact Us
               </button>
             </Link>
           </div>
