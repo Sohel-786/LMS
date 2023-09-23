38f0593264470536b568d6e7b6124d4f75e0df01
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-23 23:10:00 +0530

    Working on Sign up page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -22,29 +22,51 @@ function SignUp() {
     setViewpassword(viewPassword ? false : true);
   }
 
+  function handleChange(e) {
+    const [name, value] = e.target;
+
+    console.log(value)
+    if(name = 'avatar'){
+        setPreviewImage(value);
+    }
+
+    setSignupDetails({ ...signupDetails, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <HomeLayout>
       <div className="flex justify-center items-center h-[100vh] w-full">
         <form
+          onSubmit={handleSubmit}
           noValidate
           className="flex flex-col items-center w-[38%] p-5 shadow-formshadow bg-white rounded-xl"
         >
           <h1 className="text-4xl font-bold mb-8 text-blue-500">
             Registration
           </h1>
-          {previewImage ? (
-            <img src="" alt="" />
-          ) : (
-            <label htmlFor="avatar">
-              <FaUserCircle
-                aria-label="avatar"
-                className="w-28 h-28 cursor-pointer rounded-full border-[1px] border-red-300 hover:border-red-400 text-gray-400 hover:text-gray-600"
-              />
-              <FaEdit className="relative text-4xl -right-20 -top-8 text-red-300 cursor-pointer hover:text-red-400" />
-            </label>
-          )}
+
+          {
+                previewImage ? (
+                    <img className="w-28 h-28 rounded-full m-auto" src={previewImage} alt="" />
+                ) : (
+                    <label htmlFor="avatar">
+                    <FaUserCircle
+                        aria-label="avatar"
+                        className="w-28 h-28 cursor-pointer rounded-full border-[1px] border-red-300 hover:border-red-400 text-gray-400 hover:text-gray-600"
+                    />
+                    <FaEdit className="relative text-4xl -right-20 -top-8 text-red-300 cursor-pointer hover:text-red-400" />
+                    </label>
+                )
+          }
+
+          <input onChange={handleChange} name="avatar" type="file" id="avatar" hidden accept=".jpg , .jpeg, .webp, .png, " />
 
           <input
+            onChange={handleChange}
             className="w-[80%] p-2 bg-transparent border-b-[2px] border-sky-500 focus:outline-none focus:bg-sky-100 focus:rounded-lg placeholder:font-semibold font-bold text-green-900 my-3"
             type="text"
             name="fullname"
@@ -52,6 +74,7 @@ function SignUp() {
           />
 
           <input
+            onChange={handleChange}
             className="w-[80%] p-2 bg-transparent border-b-[2px] border-sky-500 focus:outline-none focus:bg-sky-100 focus:rounded-lg placeholder:font-semibold font-bold text-green-900 my-3"
             type="email"
             name="email"
@@ -60,6 +83,7 @@ function SignUp() {
 
           <div className="w-[80%] p-2 bg-transparent border-b-[2px] border-sky-500 focus-within:bg-sky-100 focus-within:rounded-lg text-green-900 my-3 flex">
             <input
+              onChange={handleChange}
               className="bg-transparent focus:outline-none w-full placeholder:font-semibold font-bold"
               type={viewPassword ? "text" : "password"}
               name="password"
@@ -72,7 +96,9 @@ function SignUp() {
             )}
           </div>
 
-          <button className="btn bg-blue-500 text-white font-bold border-4 border-transparent hover:bg-blue-700 hover:border-green-700 focus:scale-110 hover:scale-110 mt-5">Create Account</button>
+          <button className="btn bg-blue-500 text-white font-bold border-4 border-transparent hover:bg-blue-700 hover:border-green-700 focus:scale-110 hover:scale-110 mt-5">
+            Create Account
+          </button>
         </form>
       </div>
     </HomeLayout>
