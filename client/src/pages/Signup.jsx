a06a95e1ef63d6551cb2fa3ef025a67a9b3fabca
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-23 22:53:40 +0530

    Working on Sign up page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -27,7 +27,7 @@ function SignUp() {
       <div className="flex justify-center items-center h-[100vh] w-full">
         <form
           noValidate
-          className="flex flex-col items-center w-[40%] p-12 shadow-formshadow bg-white rounded-xl"
+          className="flex flex-col items-center w-[38%] p-5 shadow-formshadow bg-white rounded-xl"
         >
           <h1 className="text-4xl font-bold mb-8 text-blue-500">
             Registration
@@ -45,34 +45,34 @@ function SignUp() {
           )}
 
           <input
-            className="w-[80%] p-2 bg-inherit border-b-[2px] border-sky-500 focus:outline-none focus:bg-sky-100 focus:rounded-lg placeholder:font-semibold font-bold text-green-900 my-3"
+            className="w-[80%] p-2 bg-transparent border-b-[2px] border-sky-500 focus:outline-none focus:bg-sky-100 focus:rounded-lg placeholder:font-semibold font-bold text-green-900 my-3"
             type="text"
             name="fullname"
             placeholder="Enter your full name"
           />
 
           <input
-            className="w-[80%] p-2 bg-inherit border-b-[2px] border-sky-500 focus:outline-none focus:bg-sky-100 focus:rounded-lg placeholder:font-semibold font-bold text-green-900 my-3"
+            className="w-[80%] p-2 bg-transparent border-b-[2px] border-sky-500 focus:outline-none focus:bg-sky-100 focus:rounded-lg placeholder:font-semibold font-bold text-green-900 my-3"
             type="email"
             name="email"
             placeholder="Enter your Email"
           />
 
-          <div className="w-[80%] p-2 bg-inherit border-b-[2px] border-sky-500 focus:bg-sky-100 focus:rounded-lg placeholder:font-semibold font-bold text-green-900 my-3 flex">
+          <div className="w-[80%] p-2 bg-transparent border-b-[2px] border-sky-500 focus-within:bg-sky-100 focus-within:rounded-lg text-green-900 my-3 flex">
             <input
-              className="bg-inherit focus:outline-none w-full"
+              className="bg-transparent focus:outline-none w-full placeholder:font-semibold font-bold"
               type={viewPassword ? "text" : "password"}
               name="password"
               placeholder="Choose your Password"
             />
             {viewPassword ? (
-              <FiEye onClick={handlePassView} />
+              <FiEye className="text-xl" onClick={handlePassView} />
             ) : (
-              <FiEyeOff onClick={handlePassView} />
+              <FiEyeOff className="text-xl" onClick={handlePassView} />
             )}
           </div>
 
-          <button className="btn font-bold">Create Account</button>
+          <button className="btn bg-blue-500 text-white font-bold border-4 border-transparent hover:bg-blue-700 hover:border-green-700 focus:scale-110 hover:scale-110 mt-5">Create Account</button>
         </form>
       </div>
     </HomeLayout>
