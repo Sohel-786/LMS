d318e4ce7d0e5487c6e88673aa45dbde78c2a666
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-23 18:37:34 +0530

    Sign Up page started

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
 
-function SignUp(){
-    const navigate = useNavigate();
-    const [ signupDetails, setSignupDetails] = useState({
-        email : '',
-        fullname : '',
-        password : '',
-        avatar : ''
-    })
-    return(
-    )
+function SignUp() {
+  const navigate = useNavigate();
+  const [signupDetails, setSignupDetails] = useState({
+    email: "",
+    fullname: "",
+    password: "",
+    avatar: "",
+  });
+
+  return (
+    <HomeLayout>
+      <div className="flex "></div>
+    </HomeLayout>
+  );
 }
- 
+
 export default SignUp;
