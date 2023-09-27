cbb6ee32f8580a78dc8b4ee6d79d901571ce9541
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-27 15:52:36 +0530

    Added Denied Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,22 +3,21 @@ import Home from "./Pages/Home";
 import Aboutus from "./pages/Aboutus";
 import Notfound from "./pages/Notfound";
 import SignUp from "./pages/Signup";
-import SignIn from './pages/SignIn'
+import SignIn from "./pages/SignIn";
 import Contact from "./pages/Contact";
 import Denied from "./pages/Denied";
 
 function App() {
-
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<Aboutus />}/>
-        <Route path="/signup" element={<SignUp />}/>
-        <Route path="/signin" element={<SignIn />}/>
-        <Route path="/contact" element={<Contact />}/>
-        <Route path="/denied" element={<Denied />}/>
-        <Route path="*" element={<Notfound />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Aboutus />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/denied" element={<Denied />} />
+        <Route path="*" element={<Notfound />} />
       </Routes>
     </div>
   );
