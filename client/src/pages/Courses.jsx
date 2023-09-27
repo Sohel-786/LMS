eab865fbcd15ea91d9f424b033f3c99dc2b129e7
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-27 21:36:23 +0530

    Working on Course Page

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -3,6 +3,7 @@ import HomeLayout from "../layouts/HomeLayout";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCourses } from "../redux/slices/courseSlice";
 import CourseCard from "../components/CourseCard";
+import { FcApproval } from "react-icons/fc";
 
 function Courses() {
   const data = useSelector((state) => state.course.courseData);
@@ -19,13 +20,11 @@ function Courses() {
 
   return (
     <HomeLayout>
-      <section className="min-h-[90vh] flex flex-col justify-center items-center m-auto">
-        <div className="flex flex-col justify-center items-center ">
-          <h1>Our Courses</h1>
-          <p>
-            Become a Software Developer, Testing Engineer or a Data Analyst at
-            ZERO upfront fees.
-          </p>
+      <section className="min-h-[90vh] flex flex-col justify-center items-center m-auto py-6 px-7">
+        <div className="flex flex-col justify-center items-center my-7">
+          <h1 className="text-5xl font-bold tracking-wide my-2">Our Courses</h1>
+          <p className="text-xl font-semibold font-mono my-2">
+            Become a <span className="text-cyan-400 font-bold font-sans tracking-wide">Software Developer</span>, <span className="text-cyan-400 font-bold font-sans tracking-wide">Testing Engineer</span> or a <span className="text-cyan-400 font-bold font-sans tracking-wide">Data Analyst</span> at <span className="text-green-400 font-bold font-sans tracking-wide">ZERO upfront fees<FcApproval className="inline ml-1 text-2xl"/></span></p>
         </div>
 
         <div className="flex flex-wrap justify-center items-center gap-4">
