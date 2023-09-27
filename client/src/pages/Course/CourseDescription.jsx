8f334f99983ab3642f8f5154898186e7798cdedd
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-28 01:43:17 +0530

    Working on Course Description Page

--- client/src/pages/Course/CourseDescription.jsx
+++ client/src/pages/Course/CourseDescription.jsx
@@ -1,18 +1,20 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import HomeLayout from "../../layouts/HomeLayout";
 import { PiShootingStarFill } from "react-icons/pi";
-import {BsFillBookmarkStarFill} from 'react-icons/bs';
-import {RiVideoFill} from 'react-icons/ri';
+import { BsFillBookmarkStarFill } from "react-icons/bs";
+import { RiVideoFill } from "react-icons/ri";
+import { useSelector } from "react-redux";
 
 function CourseDescription() {
   const { state } = useLocation();
+  const { role, data, isLoggedIn } = useSelector((state) => state.auth.data);
+  const navigate = useNavigate();
 
   return (
     <HomeLayout>
       <div className="min-h-[90vh] flex flex-col justify-center items-center">
-
-        <div className="flex justify-center items-center flex-col bg-gradient-to-r from-stone-900 via-[#810CA8] to-blue-700 h-[300px] px-6 w-[100vw]">
-          <h1 className="flex justify-center items-center gap-2 text-5xl font-bold">
+        <div className="flex justify-center items-center flex-col bg-gradient-to-r from-stone-900 via-[#810CA8] to-blue-700 h-[300px] px-6 w-[100%] relative">
+          <h1 className="flex justify-center items-center gap-2 text-[46px] font-bold">
             <PiShootingStarFill className="text-yellow-400" />
             Become Master In {state.title}
           </h1>
@@ -25,17 +27,51 @@ function CourseDescription() {
         </div>
 
         <div className="grid grid-cols-2 justify-center items-center my-12 px-20 ">
-            <div className="rounded-xl overflow-hidden bg-[#f6ede7]">
-                <div className="rounded-xl overflow-hidden"><img src={state?.thumbnail?.secure_url} alt={'Course Thumbnail'} /></div>
-
-                <div className="flex flex-col justify-center items-center my-6">
-                    <h1 className="text-xl text-stone-800 font-bold flex justify-center items-center gap-1">Your Mentor Will Be {" "}<span className="text-yellow-600">{state.createdBy}</span><BsFillBookmarkStarFill className="text-green-600"/></h1>
-
-                    <h1 className="text-xl text-stone-800 font-bold flex justify-center items-center gap-1">There are <span className="text-yellow-600">{state.numberofLectures}</span> Lectures.<RiVideoFill className="text-red-600"/></h1>
-
-                </div>
+          <div className="rounded-xl overflow-hidden bg-[#f6ede7]">
+            <div className="rounded-xl overflow-hidden">
+              <img
+                src={state?.thumbnail?.secure_url}
+                alt={"Course Thumbnail"}
+              />
             </div>
 
+            <div className="flex flex-col justify-center items-center my-6">
+              <h1 className="text-[22px] text-stone-800 font-bold flex justify-center items-center gap-1">
+                Your Mentor Will Be{" "}
+                <span className="text-yellow-600">{state.createdBy}</span>
+                <BsFillBookmarkStarFill className="text-green-600" />
+              </h1>
+
+              <h1 className="text-xl text-slate-500 my-2 font-bold flex justify-center items-center gap-1">
+                There are{" "}
+                <span className="text-yellow-600">
+                  {state.numberofLectures}
+                </span>{" "}
+                Lectures.
+                <RiVideoFill className="text-red-600" />
+              </h1>
+            </div>
+
+            <div>
+              {role === "ADMIN" ? (
+                <button>View Lectures</button>
+              ) : data?.subscription?.status === "active" ? (
+                <button>View Lectures</button>
+              ) : (
+                <button
+                  onClick={() => {
+                    if (!isLoggedIn) {
+                      navigate("/signin");
+                    } else {
+                      navigate("/checkout");
+                    }
+                  }}
+                >
+                  Subscribe
+                </button>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </HomeLayout>
