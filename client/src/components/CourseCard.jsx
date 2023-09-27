cb9fb19f6cfff00eeeeab2bc46cb4ab4938c94a2
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-27 18:37:59 +0530

    Working on Course Page

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -1,18 +1,24 @@
 function CourseCard({ el }) {
   return (
-    <div className="w-[380px]">
-      <div>
-        <img src={el.thumbnail.secure_url} alt={"course"} />
+    <div className="w-[380px] h-[548px] rounded-lg overflow-hidden bg-white">
+      <div className="w-full h-[40%]">
+        <img
+          className="w-full aspect-auto max-h-full"
+          src={el.thumbnail.secure_url}
+          alt={"course"}
+        />
       </div>
-      <div>
-        <h1>{el.title}</h1>
-        <h3>{el.category}</h3>
-      </div>
-      <hr />
-      <div>
-        <p>{el.description}</p>
-        <p>Mentor : {el.createdBy}</p>
-        <p>Totol Lectures : {el.numberofLectures} </p>
+      <div className="flex flex-col justify-center px-6">
+        <div className="flex justify-center flex-col">
+          <h1 className="text-black font-bold text-[22px]">{el.title}</h1>
+          <h3 className="text-gray-600 font-bold text-lg font-mono tracking-wide">{el.category}</h3>
+        </div>
+        <hr className="border-t-[1px] my-3" />
+        <div>
+          <p>{el.description}</p>
+          <p>Mentor : {el.createdBy}</p>
+          <p>Totol Lectures : {el.numberofLectures} </p>
+        </div>
       </div>
     </div>
   );
