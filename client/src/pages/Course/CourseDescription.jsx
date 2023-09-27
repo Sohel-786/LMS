aa2e0863cd77c1784e6ff792460409cfbfe37dd9
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-09-28 00:25:21 +0530

    Working on Course Description Page

--- client/src/pages/Course/CourseDescription.jsx
+++ client/src/pages/Course/CourseDescription.jsx
@@ -9,8 +9,9 @@ function CourseDescription(){
     return (
         <HomeLayout>
             <div className="min-h-[90vh] flex flex-col justify-center items-center">
-                <div>
-                   <PiShootingStarFill/> <h1>Become a {state.title}</h1>
+                <div className="flex justify-center items-center flex-col">
+                    <h1 className="flex justify-center items-center gap-2 text-5xl font-bold"><PiShootingStarFill className="text-yellow-400"/>Become Master In {state.title}</h1>
+                    <h3 className="text-cyan-500 text-4xl my-6 font-bold">( Extensive Learning Course ) <span className="bg-yellow-500 px-4 py-1 text-base font-black text-stone-800 rounded-2xl">REMOTE</span></h3>
                 </div>
             </div>
         </HomeLayout>
