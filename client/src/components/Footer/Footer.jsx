f08357944a56e3d699d93fb4a298985351bc6abe
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-02 02:28:40 +0530

    Working on HomeLayout and Recreating UI

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ import Li from "./LI";
 import { useNavigate } from "react-router-dom";
 
 function Footer() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const newDate = new Date();
   const year = newDate.getFullYear();
 
@@ -28,37 +28,40 @@ function Footer() {
         </h1>
       </section>
 
-      <section className="flex flex-col justify-center w-[30%]">
-          <h1 className="text-white text-2xl font-bold tracking-wider">
-            Courses
-          </h1>
-          <ul className="flex flex-col justify-center text-xl text-gray-400 font-bold tracking-wide gap-3 mt-8">
-            <li>Full Stack Web Development</li>
-            <li>Data Analytics</li>
-            <li>Mobile Development</li>
-            <li>Software Testing & Automation</li>
-            <li>Backend Development</li>
-          </ul>
+      <section className="flex flex-col justify-center w-[35%] pl-4">
+        <h1 className="text-white text-2xl font-bold tracking-wider">
+          Courses
+        </h1>
+        <ul className="flex flex-col justify-center text-base text-gray-400 font-bold tracking-wide gap-3 mt-8">
+          <li>Full Stack Web Development</li>
+          <li>Data Analytics</li>
+          <li>Mobile Development</li>
+          <li>Software Testing & Automation</li>
+          <li>Backend Development</li>
+        </ul>
 
         <div className="w-full flex justify-center items-center bg-black mt-10 rounded-2xl h-[115px]">
           <img className="w-[22%] aspect-auto" src="/assets/arrow.gif" />
           <div className="flex flex-col justify-center">
-            <h1 className="text-white font-semibold text-2xl">Get Started</h1>
-            <p className="text-yellow-300 text-lg font-semibold">
+            <h1 className="text-white font-semibold text-xl">Get Started</h1>
+            <p className="text-yellow-300 text-base font-semibold">
               Join the Program
             </p>
           </div>
 
-          <button onClick={() => {
-            navigate('/courses')
-          }} className="text-white py-2 px-2 ml-3 rounded-md font-semibold text-lg bg-[#ed0331] hover:bg-[#ed0332eb] hover:scale-105 transition-all duration-200 hover:border-[1px] hover:border-white ease-in-out">
+          <button
+            onClick={() => {
+              navigate("/courses");
+            }}
+            className="text-white py-2 px-2 ml-2 rounded-md font-semibold text-base bg-[#ed0331] hover:bg-[#ed0332eb] hover:scale-105 transition-all duration-200 hover:border-[1px] hover:border-white ease-in-out"
+          >
             Get your course
           </button>
         </div>
       </section>
       <section className="flex flex-col justify-center">
         <h1 className="text-gray-400 font-semibold text-xl">Follow Us</h1>
-        <ul className="flex items-center justify-center gap-4 sm:gap-6 text-lg sm:text-3xl my-4">
+        <ul className="flex items-center justify-center gap-4 sm:gap-6 text-lg sm:text-2xl my-4">
           <Li>
             <BsFacebook />
           </Li>
