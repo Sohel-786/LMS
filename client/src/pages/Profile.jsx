f15483acb5915d3e2b6ec77bab0576f3ddfb59bb
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-06 00:56:18 +0530

    Working On Profile Page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,14 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { FaArrowLeftLong } from "react-icons/fa6";
-import { BiEdit } from 'react-icons/bi';
-import { TbDeviceDesktopCancel } from 'react-icons/tb';
+import { BiEdit } from "react-icons/bi";
+import { TbDeviceDesktopCancel } from "react-icons/tb";
+import { useState } from "react";
 
 function Profile() {
   const navigate = useNavigate();
   const { avatar, fullname, email, createdAt } = useSelector(
     (s) => s?.auth?.data
   );
+
+  const [editable, setEditable] = useState(false);
 
   return (
     <div className="h-[100vh] flex flex-col items-center">
@@ -23,7 +26,10 @@ function Profile() {
       </header>
 
       <div className="w-[90%] h-[80%] flex justify-center items-center shadow-profile my-8 pb-16 rounded-lg">
-        <div style={{userSelect : 'none'}} className="flex flex-col justify-center items-center w-1/2">
+        <div
+          style={{ userSelect: "none" }}
+          className="flex flex-col justify-center items-center w-1/2"
+        >
           <div className="w-full p-3">
             <div
               onClick={() => {
@@ -53,24 +59,48 @@ function Profile() {
           </div>
 
           <div className="w-full h-[80%] flex flex-col gap-5 justify-center">
-            <fieldset className="w-[80%] border-[2px] px-4 py-3 rounded-xl">
+            <fieldset
+              style={{
+                userSelect: "none",
+              }}
+              className="w-[80%] border-[2px] px-4 pb-2 rounded-xl"
+            >
               <legend className="font-slab text-stone-500 text-lg">Name</legend>
-              <h1 className="capitalize text-4xl font-roboto font-semibold tracking-wide">
-                {fullname}
-              </h1>
+
+              <input
+                spellCheck="false"
+                style={{
+                  caretColor: editable ? "green" : "transparent",
+                  borderBottom : editable ? '2px solid #4f46e5' : '2px solid transparent'
+                }}
+                contentEditable={editable}
+                name="fullname"
+                value={fullname}
+                className="capitalize text-4xl font-roboto font-semibold tracking-wide outline-none border-none focus:outline-none focus:ring-0"
+              />
             </fieldset>
 
-            <fieldset className="w-[80%] border-[2px] px-4 py-3 rounded-xl">
-              <legend className="font-slab text-stone-500 text-lg">Email</legend>
+            <fieldset className="w-[80%] border-[2px] px-4 py-3 rounded-xl cursor-not-allowed ">
+              <legend className="font-slab text-stone-500 text-lg">
+                Email
+              </legend>
               <h1 className="text-xl font-roboto font-semibold tracking-wide">
                 {email}
               </h1>
             </fieldset>
 
             <div className="w-full flex items-center gap-6 mt-8">
-              <button className="flex justify-center items-center gap-1 text-xl px-3 py-2 rounded-xl bg-gradient-to-b from-green-800 via-green-600 to-green-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-b hover:from-green-900 hover:via-green-700 hover:to-green-500"><BiEdit size={'22px'} />Edit Profile</button>
+              <button onClick={() =>{
+                setEditable(true)
+              }} className="flex justify-center items-center gap-1 text-xl px-3 py-2 rounded-xl bg-gradient-to-b from-green-800 via-green-600 to-green-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-b hover:from-green-900 hover:via-green-700 hover:to-green-500">
+                <BiEdit size={"22px"} />
+                Edit Profile
+              </button>
 
-              <button className="flex justify-center items-center gap-1 text-xl px-3 py-2 rounded-xl bg-gradient-to-b from-red-800 via-red-600 to-red-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-b hover:from-red-900 hover:via-red-700 hover:to-red-500"><TbDeviceDesktopCancel size={'22px'} />Cancel Subscription</button>
+              <button className="flex justify-center items-center gap-1 text-xl px-3 py-2 rounded-xl bg-gradient-to-b from-red-800 via-red-600 to-red-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-b hover:from-red-900 hover:via-red-700 hover:to-red-500">
+                <TbDeviceDesktopCancel size={"22px"} />
+                Cancel Subscription
+              </button>
             </div>
           </div>
         </div>
