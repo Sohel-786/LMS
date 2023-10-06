99c0ca52ddd624e2c1eb79a862665fbe9b2f3d39
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-07 00:21:53 +0530

    Working On profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -86,11 +86,12 @@ function Profile() {
       </header>
 
       <div className="w-[90%] h-[90%] sm:h-[480px] flex flex-col lg:flex-row justify-center items-center shadow-profile my-8 rounded-lg">
+
         <div
           style={{ userSelect: "none" }}
-          className="flex flex-col items-center w-full h-[50%] lg:h-full lg:items-center lg:w-1/2 lg:justify-normal"
+          className="flex flex-col w-full h-[43%] lg:h-full lg:items-center lg:w-1/2 lg:justify-normal"
         >
-          <div className="w-full p-3">
+          <div className="w-full p-3 h-[20%] sm:h-auto">
             <div
               onClick={() => {
                 navigate(-1);
@@ -100,7 +101,8 @@ function Profile() {
               <FaArrowLeftLong size={"36px"} className="text-stone-500" />
             </div>
           </div>
-          <div className="flex justify-center items-center w-full h-[90%]">
+
+          <div className="flex justify-center items-center w-full h-[80%] sm:h-auto">
             <div
               style={{
                 backgroundImage: `url(${formData.previewImage})`,
@@ -108,7 +110,7 @@ function Profile() {
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "cover",
               }}
-              className="w-[80%] h-full rounded-full border-[1px] border-transparent hover:border-pink-400 lg:w-[350px] lg:h-[350px]"
+              className="w-[230px] h-[230px] rounded-full border-[1px] border-transparent hover:border-pink-400 lg:w-[350px] lg:h-[350px]"
             >
               <label htmlFor="profile">
                 <BiSolidEdit
@@ -125,21 +127,21 @@ function Profile() {
           </div>
         </div>
 
-        <div className="flex flex-col px-5 py-4 w-full h-[50%] lg:w-1/2 lg:h-full">
-          <div className="w-full h-[20%]">
+        <div className="flex flex-col px-5 py-4 w-full h-[57%] lg:w-1/2 lg:h-full">
+          <div className="w-full sm:h-[20%]">
             <p className="float-right font-slab text-stone-500">
               Joined On {createdAt.slice(0, 10)}
             </p>
           </div>
 
-          <div className="w-full h-[80%] flex flex-col gap-5 justify-center  pb-16">
+          <div className="w-full flex flex-col gap-5 justify-center sm:pb-16 sm:h-[80%]">
             <fieldset
               style={{
                 userSelect: "none",
               }}
-              className="w-[80%] border-[2px] px-4 pb-2 rounded-xl"
+              className="w-full border-[2px] px-2 sm:px-4 pb-1 sm:pb-2 rounded-xl"
             >
-              <legend className="font-slab text-stone-500 text-lg">Name</legend>
+              <legend className="font-slab text-stone-500 sm:text-lg">Name</legend>
 
               <input
                 spellCheck="false"
@@ -154,12 +156,12 @@ function Profile() {
                 name="fullname"
                 onChange={handleChange}
                 value={formData.fullname}
-                className="capitalize text-4xl font-roboto font-semibold tracking-wide outline-none border-none focus:outline-none focus:ring-0 bg-transparent"
+                className="capitalize text-xl w-full font-roboto font-semibold tracking-wide outline-none border-none focus:outline-none focus:ring-0 bg-transparent sm:text-4xl"
               />
             </fieldset>
 
-            <fieldset className="w-[80%] border-[2px] px-4 py-3 rounded-xl cursor-not-allowed ">
-              <legend className="font-slab text-stone-500 text-lg">
+            <fieldset className="w-[90%] border-[2px] px-4 py-2 pb-3 select-none sm:select-auto sm:py-3 rounded-xl cursor-not-allowed">
+              <legend className="font-slab text-stone-500 sm:text-lg">
                 Email
               </legend>
               <h1 className="text-xl font-roboto font-semibold tracking-wide">
@@ -167,7 +169,7 @@ function Profile() {
               </h1>
             </fieldset>
 
-            <div className="w-full flex items-center gap-6 mt-8">
+            <div className="w-full flex flex-col sm:flex-row items-center gap-4 sm:gap-6 mt-4 sm:mt-8">
               {editable ? (
                 <>
                   <button
@@ -179,7 +181,7 @@ function Profile() {
                       });
                       setEnableSave(false);
                     }}
-                    className="flex justify-center items-center gap-2 text-xl px-5 py-2 rounded-xl bg-gradient-to-t from-stone-800 via-stone-600 to-stone-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-b hover:from-stone-900 hover:via-stone-700 hover:to-stone-500"
+                    className="flex justify-center items-center rounded-xl bg-gradient-to-t from-stone-800 via-stone-600 to-stone-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-b hover:from-stone-900 hover:via-stone-700 hover:to-stone-500  py-2 gap-2 px-4 sm:text-xl sm:px-5"
                   >
                     <MdFreeCancellation size={"22px"} />
                     Cancel
@@ -188,7 +190,7 @@ function Profile() {
                   <button
                     onClick={handleSubmit}
                     disabled={!enableSave}
-                    className="flex justify-center items-center gap-2 text-xl px-5 py-2 rounded-xl bg-gradient-to-b from-green-800 via-green-600 to-green-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-t hover:from-green-900 hover:via-green-700 hover:to-green-500 disabled:cursor-not-allowed disabled:bg-gradient-to-r disabled:from-red-800 disabled:to-red-500"
+                    className="flex justify-center items-center rounded-xl bg-gradient-to-b from-green-800 via-green-600 to-green-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-t hover:from-green-900 hover:via-green-700 hover:to-green-500 disabled:cursor-not-allowed disabled:bg-gradient-to-r disabled:from-red-800 disabled:to-red-500 py-2 gap-2 px-4 sm:text-xl sm:px-5"
                   >
                     <GiSave size={"22px"} />
                     Save
@@ -200,12 +202,12 @@ function Profile() {
                     onClick={() => {
                       setEditable(true);
                     }}
-                    className="flex justify-center items-center gap-1 text-xl px-3 py-2 rounded-xl bg-gradient-to-b from-green-800 via-green-600 to-green-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-t hover:from-green-900 hover:via-green-700 hover:to-green-500"
+                    className="flex justify-center items-center rounded-xl bg-gradient-to-b from-green-800 via-green-600 to-green-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-t hover:from-green-900 hover:via-green-700 hover:to-green-500 py-2 gap-2 px-4 sm:text-xl sm:px-5"
                   >
                     <BiEdit size={"22px"} />
                     Edit Profile
                   </button>{" "}
-                  <button className="flex justify-center items-center gap-1 text-xl px-3 py-2 rounded-xl bg-gradient-to-b from-red-800 via-red-600 to-red-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-t hover:from-red-900 hover:via-red-700 hover:to-red-500">
+                  <button className="flex justify-center items-center rounded-xl bg-gradient-to-b from-red-800 via-red-600 to-red-400 text-white font-bold hover:scale-110 transition-all duration-300 ease-in-out hover:bg-gradient-to-t hover:from-red-900 hover:via-red-700 hover:to-red-500 py-2 gap-2 px-4 sm:text-xl sm:px-5">
                     <TbDeviceDesktopCancel size={"22px"} />
                     Cancel Subscription
                   </button>
