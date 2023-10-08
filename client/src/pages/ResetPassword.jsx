8f5c19000e028f69afffc4f89a1e81ac81242f7e
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-09 00:24:21 +0530

    Working On Forgot Password Functionality And UI

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,26 +1,68 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { FiEye, FiEyeOff } from "react-icons/fi";
+import { isValidPassword } from "../helpers/RegexMatcher";
 
 function ResetPassword() {
-  const [ viewPasswords , setViewPasswords ] = useState({
-    viewNewPassword : false,
-    viewConfirmPassword : false
-  })
+  const [viewPasswords, setViewPasswords] = useState({
+    viewNewPassword: false,
+    viewConfirmPassword: false,
+  });
 
-  function handleChange() {}
+  const [password, setPassword] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    if (name === "confirmPassword") {
+      const confirm = document.getElementById("confirm");
+
+      if (!value) {
+        confirm.style.borderColor = "#0ea5e9";
+      } else if (!(password.password === value)) {
+        confirm.style.borderColor = "red";
+      } else {
+        confirm.style.borderColor = "#4ade80";
+      }
+    }
+    setPassword({
+      ...password,
+      [name]: value,
+    });
+  }
 
   function handleConfirmPassView() {
     setViewPasswords({
-        ...viewPasswords,
-        viewConfirmPassword : !viewPasswords.viewConfirmPassword
-    })
+      ...viewPasswords,
+      viewConfirmPassword: !viewPasswords.viewConfirmPassword,
+    });
   }
 
   function handleNewPassView() {
     setViewPasswords({
-        ...viewPasswords,
-        viewNewPassword : !viewPasswords.viewNewPassword
-    })
+      ...viewPasswords,
+      viewNewPassword: !viewPasswords.viewNewPassword,
+    });
+  }
+
+  async function handleSubmit() {
+    if (!password.password || !password.confirmPassword) {
+      toast.error("All fields are required");
+      return;
+    }
+
+    if(!(password.password === password.confirmPassword)){
+        toast.error("Confirmation of new password doesn't match");
+        return;
+    }
+
+    if(!isValidPassword(password.password)){
+        toast.error("Password must be 6 to 16 characters long with at least a number and symbol")
+        return;
+    }
   }
 
   return (
@@ -37,20 +79,19 @@ function ResetPassword() {
           />
         </div>
       </header>
-        <div
-          className="flex flex-col justify-center w-[80%] md:w-[60%] lg:w-[40%] bg-white rounded-xl py-5 px-6"
-        >
+      <div className="w-full flex justify-center items-center h-[563px]">
+        <div className="flex flex-col justify-center w-[90%] md:w-[60%] lg:w-[50%] bg-gradient-to-r from-slate-950 via-slate-800 to-slate-700 rounded-xl py-5 px-6">
           <label
             htmlFor="newPassword"
-            className="font-slab text-gray-600 mt-4 mb-2 pl-1"
+            className="font-slab tracking-wider text-gray-300 mt-4 mb-2 sm:mb-4 pl-1"
           >
             Create New Password
           </label>
 
-          <div className="w-full px-2 bg-transparent flex justify-center items-center border-[2px] border-sky-500 focus-within:border-red-600 rounded-xl ">
+          <div className="w-full px-2 bg-transparent flex justify-center items-center border-[2px] border-sky-500 focus-within:border-green-400 rounded-xl text-white">
             <input
               onChange={handleChange}
-              className="bg-transparent border-none focus:outline-0 focus:ring-0 w-full placeholder:font-semibold font-bold"
+              className="bg-transparent border-none focus:outline-0 focus:ring-0 w-full placeholder:font-semibold font-bold "
               type={viewPasswords.viewNewPassword ? "text" : "password"}
               name="password"
               id="newPassword"
@@ -75,12 +116,15 @@ function ResetPassword() {
           </div>
           <label
             htmlFor="confirmPassword"
-            className="font-slab text-gray-600 mt-4 mb-2 pl-1"
+            className="font-slab tracking-wider text-gray-300 mt-4 mb-2 sm:mb-4 pl-1"
           >
             Confirm New Password
           </label>
 
-          <div className="w-full px-2 bg-transparent flex justify-center items-center border-[2px] border-sky-500 focus-within:border-red-600 rounded-xl ">
+          <div
+            id="confirm"
+            className="w-full px-2 bg-transparent flex justify-center items-center border-[2px] border-sky-500 focus-within:border-green-400 rounded-xl text-white"
+          >
             <input
               onChange={handleChange}
               className="bg-transparent border-none focus:outline-0 focus:ring-0 w-full placeholder:font-semibold font-bold"
@@ -111,18 +155,18 @@ function ResetPassword() {
             style={{
               userSelect: "none",
             }}
-            className="w-full flex items-center gap-5 mt-1"
+            className="w-full flex items-center justify-center gap-5 mt-5"
           >
-            <button className="text-white px-6 py-2 font-roboto font-bold rounded-lg bg-gradient-to-t from-sky-800 via-sky-600 to-sky-400 hover:bg-gradient-to-t hover:from-sky-400 hover:via-sky-600 hover:to-sky-800 hover:scale-110 transition-all duration-300">
-              SUBMIT
-            </button>
-
-            <button className="text-white px-6 py-2 font-roboto font-bold rounded-lg bg-gradient-to-t from-orange-800 via-orange-600 to-orange-400 hover:bg-gradient-to-t hover:from-orange-400 hover:via-orange-600 hover:to-orange-800 hover:scale-110 transition-all duration-300">
-              CANCEL
+            <button
+              onClick={handleSubmit}
+              className="text-white px-6 py-2 font-roboto font-bold rounded-lg bg-gradient-to-t from-green-800 via-green-600 to-green-400 hover:bg-gradient-to-t hover:from-green-400 hover:via-green-600 hover:to-green-800 hover:scale-110 transition-all duration-300"
+            >
+              CHANGE PASSWORD
             </button>
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
