5356c539649004fb78b56b19e4c739479b67f983
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-10 22:53:09 +0530

    Working On Payment Gateway

--- client/src/pages/Subscribe/Checkout.jsx
+++ client/src/pages/Subscribe/Checkout.jsx
@@ -2,19 +2,19 @@ import { useEffect } from "react";
 import HomeLayout from "../../layouts/HomeLayout";
 import { GrClose } from "react-icons/gr";
 import { BiRupee } from "react-icons/bi";
-import { SiBookstack } from 'react-icons/si';
+import { SiBookstack } from "react-icons/si";
+import { FcApproval } from "react-icons/fc";
 
 function Checkout() {
   useEffect(() => {
     const id = setTimeout(() => {
       const notice = document.getElementById("notice");
-      const notice_p = document.getElementById('notice_p');
-      const notice_close = document.getElementById('notice_close');
+      const notice_p = document.getElementById("notice_p");
+      const notice_close = document.getElementById("notice_close");
 
       notice.style.height = "56px";
       notice_p.style.display = "block";
       notice_close.style.display = "block";
-
     }, 1500);
 
     return () => {
@@ -40,8 +40,8 @@ function Checkout() {
             id="notice_close"
             onClick={() => {
               const notice = document.getElementById("notice");
-              const notice_p = document.getElementById('notice_p');
-              const notice_close = document.getElementById('notice_close');
+              const notice_p = document.getElementById("notice_p");
+              const notice_close = document.getElementById("notice_close");
 
               notice.style.height = "0px";
               notice_p.style.display = "none";
@@ -52,8 +52,8 @@ function Checkout() {
           />
         </div>
 
-        <div className="w-[40%] rounded-xl overflow-hidden h-[400px] my-[50px] shadow-marquee">
-          <div className="w-full h-[45%]">
+        <div className="w-[40%] rounded-xl overflow-hidden h-[450px] my-[50px] shadow-marquee">
+          <div className="w-full h-[40%]">
             <img
               className="h-full w-full"
               src={
@@ -64,19 +64,26 @@ function Checkout() {
           </div>
 
           <div className="flex flex-col px-2">
-            <h1 className="flex items-center gap-2 text-2xl font-slab text-orange-500 my-3 px-2"> <SiBookstack  className="text-blue-500"/> Subscription bundle</h1>
+            <h1 className="flex items-center gap-2 text-2xl font-slab text-orange-500 my-3 px-2">
+              {" "}
+              <SiBookstack className="text-blue-500" /> Subscription bundle
+            </h1>
 
-            <hr className="border-b-[1.5px] rounded-xl my-1" />
+            <hr className="border-b-[1.2px] rounded-xl my-1 border-indigo-100" />
 
-            <div className="flex flex-col">
-              <h1>
-                This purchase will allow you to access all the available courses
-                on our platform for{" "}
-                <span className="font-bold text-yellow-500">
-                  1 yr duratrion
-                </span>{" "}
-                All the existing and new launched courses will be available
-              </h1>
+            <div className="flex flex-col just">
+              <div className="text-lg">
+                <FcApproval />
+                <h1>
+                  {" "}
+                  This purchase will allow you to access all the available
+                  courses on our platform for{" "}
+                  <span className="font-bold text-yellow-500">
+                    1 yr duratrion
+                  </span>{" "}
+                  All the existing and new launched courses will be available
+                </h1>
+              </div>
               <p className="flex items-center justify-center gap-1 text-2xl font-bold text-yellow-500">
                 <BiRupee />
                 <span>499</span> only
