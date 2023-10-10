1486d3cae7f50be50cf3fb8ca86cd519f650b9f1
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-10 23:36:34 +0530

    Working On Payment Gateway

--- client/src/redux/slices/paymentSlice.js
+++ client/src/redux/slices/paymentSlice.js
@@ -11,38 +11,40 @@ const initialState = {
   monthlySalesRecord: [],
 };
 
-export const getRazorpayId = createAsyncThunk('/payment/getId', async () => {
-    try{
-        const res = await axiosInstance.get('/payments/razorpay-key')
-        return res.data;
-    }catch(err){
-        toast.error('Failed to load Data');
-    }
+export const getRazorpayId = createAsyncThunk("/payment/getId", async () => {
+  try {
+    const res = await axiosInstance.get("/payments/razorpay-key");
+    return res.data;
+  } catch (err) {
+    toast.error("Failed to load Data");
+  }
 });
 
-export const purchaseCourseBundle = createAsyncThunk('/paymentpurchaseCourse', async () =>{
+export const purchaseCourseBundle = createAsyncThunk(
+  "/paymentpurchaseCourse",
+  async () => {
     try {
-        const res = await axiosInstance.post('/payments/subscribe');
-        return res.data;
+      const res = await axiosInstance.post("/payments/subscribe");
+      return res.data;
     } catch (err) {
-        toast.error(err?.response?.data?.message);
+      toast.error(err?.response?.data?.message);
     }
-});
-
+  }
+);
 
 const paymentSlice = createSlice({
   name: "payment",
   initialState: initialState,
   reducers: {},
-  extraReducers: (builder) =>{
-        builder
-        .addCase(getRazorpayId.fulfilled, (state, action) => {
-            state.key = action?.payload?.key;
-        })
-        .addCase(purchaseCourseBundle.fulfilled, (state,action) =>{
-            state.subscription_id = action?.payload?.subscription_id;
-        })
-  } 
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRazorpayId.fulfilled, (state, action) => {
+        state.key = action?.payload?.key;
+      })
+      .addCase(purchaseCourseBundle.fulfilled, (state, action) => {
+        state.subscription_id = action?.payload?.subscription_id;
+      });
+  },
 });
 
 export default paymentSlice.reducer;
