85aa7390748e886cbc3e2e5c0f01c66866f2111c
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-13 19:59:19 +0530

    Working On Lecture Display Page

--- client/src/pages/Lectures/DisplayLectures.jsx
+++ client/src/pages/Lectures/DisplayLectures.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCourseLectures } from "../../redux/slices/lectureSlice";
 import { nanoid } from "nanoid";
+import { RiDeleteBin6Fill } from "react-icons/ri";
 
 function DisplayLectures() {
   const dispatch = useDispatch();
@@ -14,11 +15,17 @@ function DisplayLectures() {
   const { role } = useSelector((s) => s?.auth);
   const [currentId, setCurrentId] = useState(0);
   const [focus, setFocus] = useState(0);
+  const [ id, setId ] = useState();
 
   useEffect(() => {
     if (!state) navigate("/courses");
     dispatch(getCourseLectures(state._id));
   }, []);
+
+  function handleLectureDelete(id){
+    const root = document.getElementById('root');
+
+  }
 
   return (
     <HomeLayout>
@@ -65,21 +72,30 @@ function DisplayLectures() {
                   className={
                     idc === focus
                       ? `w-[90%] min-h-[100px] flex items-center cursor-pointer rounded-lg shadow-marquee px-2 gap-2 scale-105 bg-cyan-500 text-white`
-
                       : `w-[90%] min-h-[100px] flex items-center cursor-pointer rounded-lg shadow-marquee px-2 bg-gray-100 gap-2 hover:scale-110 hover:bg-sky-100 transition-all duration-300 ease-in-out`
                   }
                 >
                   <video className="w-[30%] h-full">
                     <source src={el?.lecture?.secure_url} type="video/mp4" />
                   </video>
-                  <div className="flex flex-col gap-1 h-full py-3 px-1 w-[70%]">
+                  <div className="flex flex-col gap-1 h-full py-3 px-1 w-[70%] justify-between">
                     <h1 className="font-bold font-slab">
                       {el.title.length > 40
                         ? el.title.slice(0, 40) + "..."
                         : el.title}
                     </h1>
-                    <p className={`font-semibold font-roboto tracking-wide ${focus === idc ? 'text-red-800' : 'text-indigo-600'}`}>
+                    <p
+                      className={`font-semibold font-roboto tracking-wide flex justify-between items-center w-full ${
+                        focus === idc ? "text-red-800" : "text-indigo-600"
+                      }`}
+                    >
                       {el?.lecture?.duration}
+                      <RiDeleteBin6Fill
+                        onClick={() => {
+                          handleLectureDelete(el._id);
+                        }}
+                        className="text-2xl self-end text-green-700 hover:text-red-600 hover:scale-125 transition-all duration-200 ease-in-out"
+                      />
                     </p>
                   </div>
                 </div>
