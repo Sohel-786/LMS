df120f96e2f9cb7c91f06dff5d0356e06088bfb7
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-14 23:19:52 +0530

    Working On responsive Design

--- client/src/pages/Lectures/DisplayLectures.jsx
+++ client/src/pages/Lectures/DisplayLectures.jsx
@@ -73,11 +73,11 @@ function DisplayLectures() {
   return (
     <HomeLayout>
       <div className={` ${role === "ADMIN" ? "mt-14" : "mt-14"}`}>
-        <p className="flex items-center font-slab text-sm pl-12"><span className="cursor-pointer text-red-600 hover:underline hover:text-red-800" onClick={() => {
+        <p className="flex items-center font-slab text-sm pl-4 md:pl-9 lg:pl-12"><span className="cursor-pointer text-red-600 hover:underline hover:text-red-800" onClick={() => {
           navigate('/courses')
         }}>Courses</span><FiChevronRight className="text-red-600" /><span className="text-indigo-400">{state.title}</span></p>
       </div>
-      <div className={`w-full flex pl-12 pr-4 mb-2`}>
+      <div className={`w-full flex flex-col sm:flex-row px-3 md:pl-4 lg:pl-12 md:pr-2 lg:pr-4 mb-2`}>
         <div
           id="deleteLecture"
           className="hidden fixed h-[100vh] w-[100vw] top-0 right-0 left-0 bottom-0 bg-gradient-to-r from-[#0000007a] to-[#0000007a] justify-center items-center z-50"
@@ -121,26 +121,26 @@ function DisplayLectures() {
             </div>
           </div>
         </div>
-        <div className="w-[60%] ">
+        <div className=" w-full mt-5 sm:w-[60%] sm:mt-0">
           <video
             src={
               !lectures[currentId]
                 ? ""
                 : lectures[currentId]?.lecture?.secure_url
             }
-            className="w-full h-[400px]"
+            className="w-full md:h-[300px] lg:h-[390px]"
             controls
             autoPlay
           ></video>
-          <h1 className="text-3xl font-semibold my-4 font-slab">
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold my-4 font-slab">
             {lectures[currentId]?.title}
           </h1>
-          <p className="text-lg font-semibold font-sans text-gray-600">
+          <p className="text-base lg:text-lg font-semibold font-sans text-gray-600">
             {lectures[currentId]?.description}
           </p>
         </div>
 
-        <div className="w-[40%] flex flex-col">
+        <div className="w-full sm:w-[40%] flex flex-col">
           {role === "ADMIN" && (
             <button
               onClick={() => {
@@ -151,7 +151,7 @@ function DisplayLectures() {
               ADD LECTURE
             </button>
           )}
-          <div className="w-full h-[80%] overflow-y-auto flex flex-col items-center gap-3 pl-2 py-3">
+          <div className="w-full h-[80%] overflow-y-auto flex flex-col items-center gap-4 sm:gap-3 sm:pl-2 py-6 sm:py-3">
             {lectures.map((el, idc) => {
               return (
                 <div
@@ -162,7 +162,7 @@ function DisplayLectures() {
                   key={nanoid()}
                   className={
                     idc === focus
-                      ? `w-[90%] min-h-[100px] flex items-center cursor-pointer rounded-lg shadow-marquee px-2 gap-2 scale-105 bg-cyan-500 text-white`
+                      ? `w-[93%] sm:w-[90%] min-h-[100px] flex items-center cursor-pointer rounded-lg shadow-marquee px-2 gap-2 scale-105 bg-cyan-500 text-white`
                       : `w-[90%] min-h-[100px] flex items-center cursor-pointer rounded-lg shadow-marquee px-2 bg-gray-100 gap-2 hover:scale-110 hover:bg-sky-100 transition-all duration-300 ease-in-out`
                   }
                 >
@@ -170,7 +170,7 @@ function DisplayLectures() {
                     <source src={el?.lecture?.secure_url} type="video/mp4" />
                   </video>
                   <div className="flex flex-col gap-1 h-full py-3 px-1 w-[70%] justify-between">
-                    <h1 className="font-bold font-slab">
+                    <h1 className="font-bold font-slab text-sm lg:text-base">
                       {el.title.length > 40
                         ? el.title.slice(0, 40) + "..."
                         : el.title}
@@ -186,7 +186,7 @@ function DisplayLectures() {
                           onClick={() => {
                             handleShow(el);
                           }}
-                          className="text-2xl self-end text-green-700 hover:text-red-600 hover:scale-125 transition-all duration-200 ease-in-out"
+                          className="text-lg md:text-xl lg:text-2xl self-end text-green-700 hover:text-red-600 hover:scale-125 transition-all duration-200 ease-in-out"
                         />
                       )}
                     </p>
--- client/src/pages/Subscribe/CheckoutFail.jsx
+++ client/src/pages/Subscribe/CheckoutFail.jsx
@@ -2,81 +2,78 @@ import { useEffect } from "react";
 import { BsBoxArrowInRight } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import HomeLayout from "../../layouts/HomeLayout";
-import { BiSolidErrorCircle } from 'react-icons/bi';
+import { BiSolidErrorCircle } from "react-icons/bi";
 import { useDispatch } from "react-redux";
 import { getUserDetails } from "../../redux/slices/authSlice";
 
-function CheckoutFail(){
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+function CheckoutFail() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    async function load(){
-      await dispatch(getUserDetails());
-    }
-  
+  async function load() {
+    await dispatch(getUserDetails());
+  }
 
-    useEffect(() => {
+  useEffect(() => {
+    load();
 
-      load();
+    const id = setTimeout(() => {
+      const showImage = document.getElementById("showImage");
+      showImage.style.display = "none";
 
-      const id = setTimeout(() =>{
-        const showImage = document.getElementById('showImage');
-        showImage.style.display = 'none';
-  
-        const showMsg = document.getElementById('showMsg');
-        showMsg.style.display = 'flex';
-      },4000);
-  
-      return () => {
-        clearTimeout(id);
-      };
-    }, []);
+      const showMsg = document.getElementById("showMsg");
+      showMsg.style.display = "flex";
+    }, 4000);
 
+    return () => {
+      clearTimeout(id);
+    };
+  }, []);
 
-    return (
-        <HomeLayout>
-        <div className="w-full flex flex-col justify-center items-center py-20">
-          <div
-            id="showImage"
-            style={{ userSelect: "none" }}
-            className="w-full flex justify-center items-center"
-          >
-            <img
-              className="h-[400px] max-w-full"
-              src={"/assets/paymentFailed.webp"}
-              alt="gif"
-            />
-          </div>
-          <div
-            id='showMsg'
-            className="hidden flex-col justify-center items-center m-auto w-[98%] sm:w-[90%] bg-red-200 py-20 rounded-3xl"
-          >
-            <div className="flex items-center gap-2 md:gap-3 lg:gap-4 text-center text-2xl md:text-5xl lg:text-6xl font-poppins font-bold">
-              <BiSolidErrorCircle className="text-red-600 text-4xl md:text-6xl lg:text-7xl" />
-              <h1 className="text-red-600">Payment Failed</h1>
-            </div>
-  
-            <div className="flex flex-col justify-center items-center text-center ml-4 md:ml-12 lg:ml-20 mt-3 md:mt-6 lg:mt-8">
-              <h1 className="font-slab tracking-wide text-2xl md:text-[30px] lg:text-[35px] text-blue-500">
-                Please Try Again Later
-              </h1>
-            </div>
-  
-            <button
-              style={{
-                userSelect: "none",
-              }}
-              onClick={() => {
-                navigate("/");
-              }}
-              className="py-2 md:py-3 lg:py-4 my-4 md:my-8 lg:my-10 ml-6 md:ml-10 lg:ml-14 px-8 md:px-12 lg:px-16 rounded-xl bg-gradient-to-t from-green-800 via-green-600 to-green-400 text-white font-bold text-xl md:text-2xl lg:text-3xl hover:scale-110 transition-all duration-300 ease-in-out flex items-center gap-2 lg:gap-4"
-            >
-              Go to dashboard <BsBoxArrowInRight />
-            </button>
-          </div>
+  return (
+    <HomeLayout>
+      <div className="w-full flex flex-col justify-center items-center py-20">
+        <div
+          id="showImage"
+          style={{ userSelect: "none" }}
+          className="w-full flex justify-center items-center"
+        >
+          <img
+            className="h-[400px] max-w-full"
+            src={"/assets/paymentFailed.webp"}
+            alt="gif"
+          />
         </div>
-      </HomeLayout>
-    )
+        <div
+          id="showMsg"
+          className="hidden flex-col justify-center items-center m-auto w-[98%] sm:w-[90%] bg-red-200 py-20 rounded-3xl"
+        >
+          <div className="flex items-center gap-2 md:gap-3 lg:gap-4 text-center text-2xl md:text-5xl lg:text-6xl font-poppins font-bold">
+            <BiSolidErrorCircle className="text-red-600 text-4xl md:text-6xl lg:text-7xl" />
+            <h1 className="text-red-600">Payment Failed</h1>
+          </div>
+
+          <div className="flex flex-col justify-center items-center text-center ml-4 md:ml-12 lg:ml-20 mt-3 md:mt-6 lg:mt-8">
+            <h1 className="font-slab tracking-wide text-2xl md:text-[30px] lg:text-[35px] text-blue-500">
+              Please Try Again Later
+            </h1>
+          </div>
+
+          <button
+            style={{
+              userSelect: "none",
+            }}
+            onClick={() => {
+              navigate("/");
+            }}
+            className="py-2 md:py-3 lg:py-4 my-4 md:my-8 lg:my-10 ml-6 md:ml-10 lg:ml-14 px-8 md:px-12 lg:px-16 rounded-xl bg-gradient-to-t from-green-800 via-green-600 to-green-400 text-white font-bold text-xl md:text-2xl lg:text-3xl hover:scale-110 transition-all duration-300 ease-in-out flex items-center gap-2 lg:gap-4"
+          >
+            Go to dashboard <BsBoxArrowInRight />
+          </button>
+        </div>
+      </div>
+    </HomeLayout>
+  );
 }
 
 export default CheckoutFail;
