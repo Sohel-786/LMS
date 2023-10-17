bab280c23ab69569bf8f7edf761d5b64d283506b
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-17 21:46:17 +0530

    Working On Admin Dashboard

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,13 +1,29 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import HomeLayout from '../layouts/HomeLayout';
 import { useNavigate } from 'react-router-dom';
+import { getPaymentRecords } from '../redux/slices/paymentSlice';
+import { getStatsData } from '../redux/slices/statsSlice';
+import { useEffect } from 'react';
+import { getAllCourses } from '../redux/slices/courseSlice';
 
 function AdminDashboard(){
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const {  }
+    const { allUserCount, subscriberCount } = useSelector((s) => s?.stat);
+    const { allPayments, finalMonths, monthlySalesRecord } = useSelector((s) => s?.payment)
+    const courses = useSelector((s) => s?.course?.courseData);
+
+    async function load(){
+        await dispatch(getPaymentRecords());
+        await dispatch(getStatsData());
+        await dispatch(getAllCourses())
+    }
+
+    useEffect(() => {
+        load();
+    }, []);
 
     return(
         <HomeLayout>
--- client/src/redux/slices/paymentSlice.js
+++ client/src/redux/slices/paymentSlice.js
@@ -67,9 +67,17 @@ export const cancelSubscription = createAsyncThunk('payment/cancelSubscription',
 export const getPaymentRecords = createAsyncThunk('/payment/records', async () => {
   try {
      const res = axiosInstance.get('/payments?count=100');
-     toast
+     toast.promise(res, {
+      loading : 'Getting Payment Records',
+      success : (data) => {
+        return data?.data?.message
+      },
+      error : 'Failed to get payment records'
+     })
+     
+     return (await res).data;
   } catch (e) {
-    
+    toast.error(e?.response?.data?.message);
   }
 })
 
@@ -92,6 +100,11 @@ const paymentSlice = createSlice({
       .addCase(verifyPayment.rejected, (state, action) => {
         toast.error(action?.payload?.message);
         state.isPaymentVerified = action?.payload?.success;
+      })
+      .addCase(getPaymentRecords.fulfilled, (state, action) => {
+        state.allPayments = action?.payload?.allPayments;
+        state.monthlySalesRecord = action?.payload?.monthlySalesRecord;
+        state.finalMonths = action?.payload?.finalMonths;
       })
   },
 });
