0ddb23feae00ac47e794b0ded4ad51a6d9092b7d
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-10-22 00:53:39 +0530

    fixed a bit

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -3,7 +3,7 @@ import HomeLayout from "../layouts/HomeLayout";
 function Loading(){
     return(
         <HomeLayout>
-            <div className="w-full min-h-[70vh] flex justify-center items-center">
+            <div className="w-full min-h-[90vh] flex justify-center items-center">
                 <div className="w-fit animate-spin">
                     <img className="w-[50px] aspect-auto" src="/assets/loading.png" alt="Loading" />
                 </div>
