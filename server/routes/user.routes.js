470cd38cdff0f99ad72166968b2afe3f5abbc9c0
Sohel Shaikh <sohel.shaikh27100@gmail.com>
2023-12-09 21:56:05 +0530

    Fixed A bit

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,28 +1,37 @@
-import express from 'express';
+import express from "express";
 const userRoutes = express.Router();
 
-import { register, login, logout, getUser, resetPassword, forgotPassword, changePassword, updateProfile, contactUs} from '../controllers/user.controllers.js';
+import {
+  register,
+  login,
+  logout,
+  getUser,
+  resetPassword,
+  forgotPassword,
+  changePassword,
+  updateProfile,
+  contactUs,
+} from "../controllers/user.controllers.js";
 
-import {IsLoggedIn} from '../middlewares/auth.middleware.js';
-import upload from '../middlewares/multer.middlerware.js';
+import { IsLoggedIn } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/multer.middlerware.js";
 
-userRoutes.post('/register', upload.single('avatar') ,register);
+userRoutes.post("/register", upload.single("avatar"), register);
 
-userRoutes.post('/login', login);
+userRoutes.post("/login", login);
 
-userRoutes.get('/logout', IsLoggedIn, logout);
+userRoutes.get("/logout", IsLoggedIn, logout);
 
-userRoutes.get('/me', IsLoggedIn, getUser);
+userRoutes.get("/me", IsLoggedIn, getUser);
 
-userRoutes.post('/reset', forgotPassword);
+userRoutes.post("/reset", forgotPassword);
 
-userRoutes.post('/reset/:resetToken', resetPassword);
+userRoutes.post("/reset/:resetToken", resetPassword);
 
-userRoutes.post('/changepassword',IsLoggedIn, changePassword);
+userRoutes.post("/changepassword", IsLoggedIn, changePassword);
 
-userRoutes.post('/update', IsLoggedIn, upload.single('avatar'), updateProfile);
+userRoutes.post("/update", IsLoggedIn, upload.single("avatar"), updateProfile);
 
-userRoutes.post('/contactUs', contactUs);
-
+userRoutes.post("/contactUs", contactUs);
 
 export default userRoutes;
